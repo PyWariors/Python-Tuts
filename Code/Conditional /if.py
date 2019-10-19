81afb007573d03349a2fe3befdeb7e21e9964315
Uma Subbiah <auriflore1@gmail.com>
2019-10-19 19:50:53 +0530

    Added conditional programs

---
+++ Code/Conditional /if.py
@@ -0,0 +1,9 @@
+# Basic conditional function is provided by the if statement
+
+# Conditions are written using > < == (note) <= >= and !=
+
+
+a = 7
+
+if(a == 7):
+    print(a)
---
+++ Code/Conditional /if_elif_else.py
@@ -0,0 +1,10 @@
+# Intermedite else statements (between the if and the else are specified using elif)
+
+a = 5
+
+if( a>5 ):
+    print("Greater")
+elif (a<5):
+    print("Less")
+else:
+    print("Equal")
